bb7ae57012e1634ded604eb74c7cc8c873c653b8
SomeonesDads <nael.i.manurung@gmail.com>
2026-05-14 20:48:55 +0700

    feat: add header component

---
+++ frontend/components/layout/HeaderBar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import Image from "next/image";
+
+export default function HeaderBar() {
+  return (
+    <header
+      className="fixed top-0 left-0 right-0 z-40 flex items-center justify-start h-[42px] px-4"
+      style={{
+        backgroundColor: "#27231d",
+        borderBottom: "1px solid #27241e",
+      }}
+    >
+      <Image
+        src="/images/CkckLogodark.png"
+        alt="CkCk Logo"
+        width={80}
+        height={28}
+        className="object-contain"
+        priority
+      />
+      <span
+        className="text-xs tracking-widest uppercase font-sora font-medium ml-3"
+        style={{ color: "#c7bcaa" }}
+      >
+        GENERIC SLOGAN
+      </span>
+    </header>
+  );
+}
